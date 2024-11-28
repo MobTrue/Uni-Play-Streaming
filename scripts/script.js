1667f3a43c8bdfa21d657c7466c7208ef60f97fd
MobTrue <emersomobernardini@outlook.com>
2024-11-28 20:40:33 -0300

    Change layouts in information section and improve page routing

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,19 @@
  // Array de planos Next Play
 let nextPlans = [
-   { id: 1, name: "Next Play - Mensal", preco: 34.90, time: "1 mês"},
-   { id: 2, name: "Next Play - Mensal 2 telas", preco: 47.90, time: "1 mês, 2 telas"},
-   { id: 3, name: "Next Play - Bimestral", preco: 59.90, time: "2 meses" },
-   { id: 4, name: "Next Play - Trimestral", preco: 89.90, time: "3 meses" },
-   { id: 5, name: "Next Play - Semestral", preco: 149.90, time: "6 meses"},
-   { id: 6, name: "Next Play - Anual", preco: 259.90, time: "12 meses" }
+   { id: 1, name: "Next Play - Mensal", price: 34.90, time: "1 mês"},
+   { id: 2, name: "Next Play - Bimestral", price: 59.90, time: "2 meses" },
+   { id: 3, name: "Next Play - Trimestral", price: 89.90, time: "3 meses" },
+   { id: 4, name: "Next Play - Semestral", price: 149.90, time: "6 meses"},
+   { id: 5, name: "Next Play - Anual", price: 259.90, time: "12 meses" }
 ];
 
 // Array de planos Uni Mais
 let uniPlans = [
-   { id: 1, name: "Uni Mais - Mensal", preco: 24.90, time: "1 mês"},
-   { id: 2, name: "Uni Mais - Mensal 2 telas", preco: 37.90, time: "1 mês, 2 telas"},
-   { id: 3, name: "Uni Mais - Bimestral", preco: 49.90, time: "2 meses" },
-   { id: 4, name: "Uni Mais - Trimestral", preco: 79.90, time: "3 meses" },
-   { id: 5, name: "Uni Mais - Semestral", preco: 139.90, time: "6 meses" },
-   { id: 6, name: "Uni Mais - Anual", preco: 249.90, time: "12 meses" }
+   { id: 1, name: "Uni Mais - Mensal", price: 24.90, time: "1 mês"},
+   { id: 2, name: "Uni Mais - Bimestral", price: 49.90, time: "2 meses" },
+   { id: 3, name: "Uni Mais - Trimestral", price: 79.90, time: "3 meses" },
+   { id: 4, name: "Uni Mais - Semestral", price: 139.90, time: "6 meses" },
+   { id: 5, name: "Uni Mais - Anual", price: 249.90, time: "12 meses" }
 ];
 
 // Função para criar cards de planos usando arrow functions
@@ -28,7 +26,7 @@ const createPlanCards = (plans, containerId) => {
       card.innerHTML = `
          <h3>${plan.name}</h3>
          <p>Duração: ${plan.time}</p>
-         <p>R$${plan.preco.toFixed(2)}</p>
+         <p>R$${plan.price.toFixed(2)}</p>
          
          
          <a id="classA" href="http://bit.ly/4fOky9q">
