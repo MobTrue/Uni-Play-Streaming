741e38278f29a6786b23213f4b8902355aeec25c
MobTrue <emersomobernardini@outlook.com>
2025-01-18 14:05:17 -0300

    general reconstruction of the site

---
+++ src/scripts/script.js
@@ -0,0 +1,9 @@
+const planCards = document.querySelectorAll('.plan-card');
+let currentIndex = 0;
+function updateFirstClass() {
+  planCards.forEach(card => card.classList.remove('first'));
+  planCards[currentIndex].classList.add('first');
+  currentIndex = (currentIndex + 1) % planCards.length;
+}
+updateFirstClass();
+setInterval(updateFirstClass, 5000);
