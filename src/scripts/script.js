d2d1c4546ece34ee8c1dfcefa007048f10e14f4c
MobTrue <emersomobernardini@outlook.com>
2025-05-11 01:53:10 -0300

    Movies section update 1.0

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,8 @@
 const planCards = document.querySelectorAll('.plan-card');
 let currentIndex = 0;
+
 function updateFirstClass() {
-  planCards.forEach(card => card.classList.remove('first'));
-  planCards[currentIndex].classList.add('first');
-  currentIndex = (currentIndex + 1) % planCards.length;
+    planCards.forEach(card => card.classList.remove('first'));
+    planCards[currentIndex].classList.add('first');
+    currentIndex = (currentIndex + 1) % planCards.length;
 }
-updateFirstClass();
-setInterval(updateFirstClass, 3000);
